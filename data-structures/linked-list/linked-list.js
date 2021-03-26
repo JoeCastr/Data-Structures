f5a687c2dfb410d1672864492ec5a6dc944a4bbc
Joe Castrigno <jcastrigno@gmail.com>
2021-03-26 08:19:50 -0600

    more tests added for linked-list

--- data-structures/linked-list/linked-list.js
+++ data-structures/linked-list/linked-list.js
@@ -26,6 +26,20 @@ class LinkedList {
 
     return count;
   }
+
+  // create a function that will retrieve a node containing specified data
+  returnNodeWith(data, head) {
+    // iterate through each node, starting at the head
+    // once the currentNode.data = data
+    // // return the currentNode object
+    let current = head
+
+    while (current && current.data != data) {
+      current = current.next;
+    }
+
+    return current ? current : "No such node exists in this list"
+  }
 }
 
 module.exports = {
--- data-structures/linked-list/linked-list.test.js
+++ data-structures/linked-list/linked-list.test.js
@@ -2,7 +2,31 @@ const linkedListClasses = require('./linked-list');
 const { node, linkedList } = linkedListClasses;
 // const linkedListNodeInstance = new linkedListClasses.Node();
 
-test('creates a node with data === 5', () => {
-  const exampleNode = new node(5)
-  expect(exampleNode.data).toBe(5)
+let nodeA = new node("A");
+let nodeB = new node("B");
+let nodeC = new node("C");
+let nodeD = new node("D");
+
+test('new node(5) creates a node with data === 5', () => {
+  const exampleNode = new node(5);
+  expect(exampleNode.data).toBe(5);
 });
+
+test('new linkedList(nodeA, nodeB, nodeC, nodeD) creates a list with countNodes(nodeA) === 4', () => {
+  const exampleList = new linkedList(nodeA, nodeB, nodeC, nodeD);
+  expect(exampleList.countNodes(nodeA)).toBe(4);
+});
+
+test('find a node with data value "C" returns an object', () => {
+  const exampleList = new linkedList(nodeA, nodeB, nodeC, nodeD);
+  let testFunction = exampleList.returnNodeWith("C", nodeA)
+  let result = typeof testFunction === 'object';
+  expect(result).toBe(true);
+});
+
+test('find a node with data value "C" returns an object with data property === "C"', () => {
+  const exampleList = new linkedList(nodeA, nodeB, nodeC, nodeD);
+  let testFunction = exampleList.returnNodeWith("C", nodeA)
+  let result = testFunction.data === "C"
+  expect(result).toBe(true);
+})
